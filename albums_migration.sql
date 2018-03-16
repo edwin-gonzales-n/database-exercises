86b2d759e389fe0c710391dcebd3db19c3c82198
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-16 16:13:14 -0500

    adding 2 out of 4 exercises

---
+++ albums_migration.sql
@@ -0,0 +1,63 @@
+USE codeup_test_db;
+
+/*
+drop table albums if exists;
+create table albums(
+id int not null auto_increment,
+artist varchar(250),
+name varchar(100),
+release_date date,
+sales float,
+genre varchar(100),
+content text not null,
+primary key(id)); */
+
+/* create table contacts(
+id INT NOT NULL AUTO_INCREMENT,
+usernumber varchar(100),
+name varchar(100),
+updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+created_at TIMESTAMP not null default now(),
+email varchar(320),
+content text not null,
+primary key(id));  */
+
+/*create table pizza_order(
+id INT NOT NULL AUTO_INCREMENT,
+user_name varchar(100),
+phone_number varchar(100),
+address varchar(255),
+pizza_size varchar(100),
+total_cost int not null,
+description varchar(100),
+isDelivered tinyint,
+updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+created_at TIMESTAMP not null default now(),
+email varchar(10),
+content text not null,
+primary key(id));*/
+/*
+insert into albums(
+artist, name, genre,content) 
+values
+(
+"Tommy Olivencia", 
+"Introducing Simon Perez", 
+"Salsa", 
+"One of the greates albums from Tommy Olivencia")
+,
+(
+"Tromboranga",
+"La Bemba", 
+"Salsa", 
+"New album from Tromboranga")
+,
+(
+"Jose Alberto",
+"Sueno Contigo",               
+"Salsa",
+"A classic in the Salsa genre");*/
+
+
+
+
---
+++ albums_seeder.sql
@@ -0,0 +1,27 @@
+use codeup_test_db;
+insert into albums(
+artist, name, release_date,genre,sales) 
+values
+("Various artists","Grease: The Original Soundtrack from the Motion Picture","1978","Soundtrack","38"),
+("Led Zeppelin","Led Zeppelin IV","1971","Hard rock, Heavy metal, Folk","37"),
+("Michael Jackson","Bad","1987","Pop, Funk, Rock","35"),
+("Alanis Morissette","Jagged Little Pill","1995","Alternative rock","33"),
+("Shania Twain","Come On Over","1997","Country, Pop","33"),
+("Celine Dion","Falling into You","1996","Pop, Soft rock","32"),
+("The Beatles","Sgt. Pepper's Lonely Hearts Club Band","1967","Rock","32"),
+("Eagles","Hotel California","1976","Rock, Soft rock, Folk rock","32"),
+("Various artists","Dirty Dancing","1987","Pop, Rock, R&B","32"),
+("Adele","21","2011","Pop, Soul","31"),
+("Celine Dion","Let's Talk About Love","1997","Pop, Soft rock","31"),
+("The Beatles","1","2000","Rock","31"),
+("Michael Jackson","Dangerous","1991","Rock,Funk,Pop","30"),
+("Madonna","The Immaculate Collection","1990","Pop, Dance","30"),
+("The Beatles","Abbey Road","1969","Rock","30"),
+("Bruce Springsteen","Born in the U.S.A.","1984","Rock","30"),
+("Dire Straits","Brothers in Arms","1985","Rock, Pop","30"),
+("James Horner","Titanic: Music from the Motion Picture","1997","Soundtrack","30"),
+("Metallica","Metallica","1991","Thrash metal, Heavy metal","30"),
+("Nirvana","Nevermind","1991","Grunge, Alternative rock","30"),
+("Pink Floyd","The Wall","1979","Progressive rock","30"),
+("Santana","Supernatural","1999","Rock","30"),
+("Guns N' Roses","Appetite for Destruction","1987","Hard rock","30");
