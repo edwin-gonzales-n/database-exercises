3d0d93983535f7c7f4e3c94a0183addd4fa5e9ae
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-20 11:18:48 -0500

    adding functions exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,9 @@
+use employees;
+
+select concat(first_name, last_name) 
+from employees 
+where last_name like "e%" or last_name like "%e" 
+order by emp_no desc;
+
+select datediff(curdate(),hire_date),first_name,last_name 
+from employees limit 10;
