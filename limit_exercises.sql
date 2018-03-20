3fcbe749608a0b6c8f6cf1775a14988507bfc959
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-20 10:51:10 -0500

    adding limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,8 +5,20 @@ FROM employees
 GROUP BY first_name
 ORDER BY COUNT(*) DESC
 LIMIT 10;
-
 select * from employees where last_name like "z%" limit 10 offset 50;
 
+select distinct last_name 
+from employees  
+order by last_name 
+desc limit 10;
+
+select * from employees 
+where hire_date like "199%-%-%" and birth_date like "%-12-25" 
+order by birth_date,hire_date 
+desc limit 5;
+
+select * from employees 
+where hire_date like "199%-%-%" and birth_date like "%-12-25" 
+order by birth_date,hire_date desc limit 5 offset 45;
 
 
