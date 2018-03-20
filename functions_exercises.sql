947fbe03a7a14f76b989a09852b8a62db2c3bbb7
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-20 16:09:46 -0500

    adding final changes to today's exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -5,5 +5,5 @@ from employees
 where last_name like "e%" or last_name like "%e" 
 order by emp_no desc;
 
-select datediff(curdate(),hire_date),first_name,last_name 
+select datediff(curdate(),hire_date),first_name,last_name
 from employees limit 10;
