8941e622643407cb568aafadddf64d517c0ec370
Gonzo <edwin.gonzales.n@gmail.com>
2018-03-20 02:27:20 -0500

    adding order_by_exercises.sql

---
+++ order_by_exercises.sql
@@ -0,0 +1,7 @@
+use employees;
+
+select * from employees where first_name in ('irena','vidya','maya') order by first_name;
+select * from employees where first_name in ('irena','vidya','maya') order by first_name asc, last_name asc;
+select * from employees where first_name in ('irena','vidya','maya') order by last_name  asc, first_name asc;
+select * from employees where last_name like "e%" or last_name like "%e" order by emp_no desc;
+select * from employees where hire_date like "199%-%-%" and birth_date like "%-12-25" order by hire_date asc;
